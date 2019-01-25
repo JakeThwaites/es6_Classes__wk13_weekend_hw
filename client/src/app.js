707ce8aad33056db204d7f3fbb3cb0eac2b58151
Jake Thwaites <jakethwaites@hotmail.co.uk>
2019-01-25 14:52:01 +0000

    refactored instrument_familes and select_view classes

---
+++ client/src/app.js
@@ -0,0 +1,20 @@
+const InstrumentFamilies = require('./models/instrument_families.js');
+const SelectView = require('./views/select_view.js');
+const InstrumentFamilyView = require('./views/instrument_family_view.js');
+const data = require('./data/instrument_families.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selectElement = document.querySelector('select#instrument-families');
+  // const familyDropdown = new SelectView(selectElement);
+  // familyDropdown.bindEvents();
+
+  SelectView.bindEvents(selectElement);
+
+  const familyContainer = document.querySelector('div#instrument-family');
+  // InstrumentFamilyView.bindEvents(familyContainer)
+
+  const instrumentFamilyView = new InstrumentFamilyView(familyContainer);
+  instrumentFamilyView.bindEvents();
+
+  InstrumentFamilies.bindEvents(data);
+});
---
+++ client/src/models/instrument_families.js
@@ -0,0 +1,47 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+class InstrumentFamilies {
+  constructor(data) {
+    this.data = data;
+  }
+
+
+  // const InstrumentFamilies = function (data) {
+  //   this.data = data;
+  // };
+
+  static bindEvents(data){
+    this.data = data;
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  }
+
+
+
+  // InstrumentFamilies.prototype.bindEvents = function () {
+  //   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+  //
+  //   PubSub.subscribe('SelectView:change', (evt) => {
+  //     const selectedIndex = evt.detail;
+  //     this.publishFamilyDetail(selectedIndex);
+  //   });
+  // };
+
+
+  static publishFamilyDetail(selectedIndex){
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  }
+
+  // InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+  //   const selectedFamily = this.data[selectedIndex];
+  //   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  // };
+
+}
+
+module.exports = InstrumentFamilies;
